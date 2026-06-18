b8a1f1384b28ee8eebad02adf61fa6ce7b18d6c4
Rahib-Rahman <rrahib778@gmail.com>
2026-06-19 05:10:12 +0600

    updated

--- server/db.js
+++ server/db.js
@@ -16,7 +16,7 @@ const pool = new Pool(
             host: process.env.DB_HOST,
             database: process.env.DB_NAME,
             password: process.env.DB_PASSWORD,
-            port: Number(process.env.DB_PORT) || 5433
+            port: Number(process.env.DB_PORT) || 5432
         }
 );
 
