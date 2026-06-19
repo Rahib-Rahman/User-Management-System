65f9b4bedf9f378a43848b0d88c5c4ebad906f10
Rahib-Rahman <rrahib778@gmail.com>
2026-06-19 09:20:06 +0600

    updated

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://rahib-user-management-system.onrender.com/',
+    baseURL: import.meta.env.VITE_API_URL || 'https://rahib-user-management-system.onrender.com/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
